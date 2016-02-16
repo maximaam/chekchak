d4036a9113ac782d40fb54cc110031e3a742cf21
Maamar Khemissi <maamar.khemissi@meingutscheincode.com>
2016-02-16 16:37:52 +0100

    second

--- index.php
+++ index.php
@@ -7,18 +7,14 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-<title>Test</title>
+<title>Chekchak-Biliken Joailleries</title>
 <meta name="description" content="Simple Responsive Template is a template for responsive web design. Mobile first, responsive grid layout, toggle menu, navigation bar with unlimited drop downs, responsive slideshow">
 <meta name="keywords" content="">
 
 <!-- Mobile viewport -->
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
 
-<link rel="shortcut icon" href="images/favicon.ico"  type="image/x-icon">
-
-<!-- CSS-->
-<!-- Google web fonts. You can get your own bundle at http://www.google.com/fonts. Don't forget to update the CSS accordingly!-->
-<link href='http://fonts.googleapis.com/css?family=Droid+Serif|Ubuntu' rel='stylesheet' type='text/css'>
+<link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
 
 <link rel="stylesheet" href="css/normalize.css">
 <link rel="stylesheet" href="js/flexslider/flexslider.css">
@@ -38,12 +34,36 @@
     <header class="wrapper clearfix">
 		       
         <div id="banner">        
-        	<div id="logo"><a href="basic.html"><img src="images/basic-logo.svg" alt="logo"></a></div> 
+        	<div id="logo">
+                <a href="/">
+                    <img src="images/chekchak-biliken-logo.png" alt="logo">
+                </a>
+            </div>
         </div>
         
         <!-- main navigation -->
         <nav id="topnav" role="navigation">
-          <div class="menu-toggle">Menu</div>  
+            <div class="menu-toggle">Menu</div>
+            <ul class="srt-menu" id="menu-main-navigation">
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">A propos</a></li>
+                <li>
+                    <a href="#">Selection</a>
+                    <ul>
+                        <li>
+                            <a href="#">Bijoux catégorie 1</a>
+                            <a href="#">Bijoux catégorie 2</a>
+                            <a href="#">Bijoux catégorie 3</a>
+                        </li>
+                    </ul>
+
+                </li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+
+
+
+            <!--
           <ul class="srt-menu" id="menu-main-navigation">
               <li class="current"><a href="basic.html">Home page</a></li>
               <li><a href="basic-internal.html">Internal page demo</a></li>
@@ -76,7 +96,10 @@
               <li>
                   <a href="#">menu item 5</a>
               </li>	
-          </ul>     
+          </ul>
+            -->
+
+
 		</nav><!-- end main navigation -->
   
     </header><!-- end header -->
@@ -86,31 +109,43 @@
     <section id="hero" class="clearfix">    
     <!-- responsive FlexSlider image slideshow -->
     <div class="wrapper">
-       <div class="row"> 
+       <div class="row">
+
         <div class="grid_5">
-            <h1>Simple Responsive Template</h1>
-            <p>...is a template for responsive web design. A small set of tools and best practices that allows web designers to build responsive websites faster. Websites built with Simple Responsive Template will be optimized for screen widths between 320px and anything. Resize your browser to check it out.
+            <h1>Chekchak-Biliken Joailleries</h1>
+            <p>
+                At nunc si ad aliquem bene nummatum tumentemque ideo honestus advena salutatum introieris, primitus tamquam exoptatus suscipieris et interrogatus multa coactusque mentiri, miraberis numquam antea visus summatem virum tenuem te sic enixius observantem, ut paeniteat ob haec bona tamquam praecipua non vidisse ante decennium Romam.
+                <br><br>
+                Haec ubi latius fama vulgasset missaeque relationes adsiduae Gallum Caesarem permovissent, quoniam magister equitum longius ea tempestate distinebatur, iussus comes orientis Nebridius contractis undique militaribus copiis ad eximendam periculo civitatem amplam et oportunam studio properabat ingenti, quo cognito abscessere latrones nulla re amplius memorabili gesta, dispersique ut solent avia montium petiere celsorum.
             </p>
-            <p><a href="#" class="buttonlink">CALL TO ACTION</a> <a href="#" class="buttonlink">ONE MORE</a></p>
         </div>
+
         <div class="grid_7 rightfloat">
               <div class="flexslider">
                   <ul class="slides">
                       <li>
-                          <img src="images/basic-pic1.jpg" />
-                          <p class="flex-caption">Love Brazil !!! Sea view from Rio de Janeiro fort.</p>
+                          <img src="images/bijoux/bijou1.jpg" />
+                          <p class="flex-caption">Bijou 1</p>
                       </li>
                       <li>
-                          <a href="http://www.prowebdesign.ro"><img src="images/basic-pic2.jpg" /></a>
-                          <p class="flex-caption">El Arco Cabo Mexico. This image is wrapped in a link.</p>
+                          <img src="images/bijoux/bijou2.jpg" />
+                          <p class="flex-caption">Bijou 2</p>
                       </li>
                       <li>
-                          <img src="images/basic-pic3.jpg" />
-                          <p class="flex-caption">Arches National Park, Utah, Usa.</p>
+                          <img src="images/bijoux/bijou3.jpg" />
+                          <p class="flex-caption">Bijou 3</p>
                       </li>
                       <li>
-                          <img src="images/basic-pic4.jpg" />
-                          <p class="flex-caption">Morocco desert.</p>
+                          <img src="images/bijoux/bijou4.jpg" />
+                          <p class="flex-caption">Bijou 4</p>
+                      </li>
+                      <li>
+                          <img src="images/bijoux/bijou5.jpg" />
+                          <p class="flex-caption">Bijou 5</p>
+                      </li>
+                      <li>
+                          <img src="images/bijoux/bijou6.jpg" />
+                          <p class="flex-caption">Bijou 6</p>
                       </li>
                   </ul>
                 </div><!-- FlexSlider -->
@@ -129,85 +164,43 @@
     
 <!-- content area -->    
 	<section id="content" class="wide-content">
-      <div class="row">	
-        <div class="grid_4">
-        	<h1 class="first-header">Brazil!</h1>
-            <img src="images/basic-pic1.jpg" />
+      <div class="row flex">
+        <div class="grid_4 white-box-round">
+        	<h2 class="first-header">Bijoux catégorie 1</h2>
+            <img src="images/bijoux/bijou1.jpg" />
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
         </div>
         
-        <div class="grid_4">
-        	<h1 class="first-header">Mexico!</h1>
-            <img src="images/basic-pic2.jpg" />
+        <div class="grid_4 white-box-round">
+        	<h2 class="first-header">Bijoux catégorie 2</h2>
+            <img src="images/bijoux/bijou2.jpg" />
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
         </div>
         
-        <div class="grid_4">
-        	<h1 class="first-header">US!</h1>
-            <img src="images/basic-pic3.jpg" />
+        <div class="grid_4 white-box-round">
+        	<h2 class="first-header">Bijoux catégorie 3</h2>
+            <img src="images/bijoux/bijou3.jpg" />
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
         </div>
 	  </div><!-- end row -->
-	</section><!-- end content area -->   
-      
-      
-    <!-- columns demo, delete it!-->
-    <section id="columnsdemo" style="margin-bottom:60px; width:100%" class="clearfix">
-        <h2 style="width:100%; clear:both">Columns demo</h2>
-        <div class="grid_12">12</div>
-        
-        <div class="grid_1">1</div>
-        <div class="grid_11">11</div>
-        
-        <div class="grid_2">2</div>
-        <div class="grid_10">10</div>
-        
-        <div class="grid_3">3</div>
-        <div class="grid_9">9</div>
-        
-        <div class="grid_4">4</div>
-        <div class="grid_8">8</div>
-        
-        <div class="grid_5">5</div>
-        <div class="grid_7">7</div>
-        
-        <div class="grid_6">6</div>
-        <div class="grid_6">6</div>
-                
-        <div class="grid_4">4</div>
-        <div class="grid_4">4</div>
-        <div class="grid_4">4</div>
-        
-        <div class="grid_1">1</div>
-        <div class="grid_2">2</div>
-        <div class="grid_3">3</div>
-        <div class="grid_3">3</div>
-        <div class="grid_3">3</div>   
-    </section>
-    <!-- end columns demo -->  
-    
+	</section><!-- end content area -->
       
   </div><!-- #end div #main .wrapper -->
 
 
-<!-- footer area -->    
+
 <footer>
 	<div id="colophon" class="wrapper clearfix">
-    	footer stuff
+    	Adresse...<br>Facebook...etc
     </div>
-    
-<!--You can NOT remove this attribution statement from any page, unless you get the permission from prowebdesign.ro--><div id="attribution" class="wrapper clearfix" style="color:#666; font-size:11px;">Site built with <a href="http://www.prowebdesign.ro/simple-responsive-template/" target="_blank" title="Simple Responsive Template is a free software by www.prowebdesign.ro" style="color:#777;">Simple Responsive Template</a></div><!--end attribution-->
-    
-</footer><!-- #end footer area --> 
+</footer>
 
 
-<!-- jQuery -->
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="js/libs/jquery-1.9.0.min.js">\x3C/script>')</script>
 
 <script defer src="js/flexslider/jquery.flexslider-min.js"></script>
 
-<!-- fire ups - read this file!  -->   
 <script src="js/main.js"></script>
 
 </body>
